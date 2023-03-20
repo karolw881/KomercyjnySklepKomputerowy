165675b97184617b77a2ea8ed866ae2d6e2aa44a
MICHU_KFC <michal.pieciun@protonmail.com>
2023-03-20 23:21:48 +0100

    Podlinkowałem footer do każdej podstrony

--- src/pages/Podzespoly/Podzespoly.js
+++ src/pages/Podzespoly/Podzespoly.js
@@ -1,11 +1,13 @@
 import NavBar from "../../Index-components/Navbar";
 import Categories from "../../Index-components/Categories";
+import Footer from "../../Index-components/Footer";
 
 const Podzespoly = () => {
     return ( 
         <>
         <NavBar/>
         <Categories/>
+        <Footer/>
         </>
     );
 }

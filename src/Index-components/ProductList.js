b93de40d60cdf57542cef39c445d5b6c4deed7de
Michsen <xmichsen@gmail.com>
2023-03-21 20:42:08 +0100

    Nie pamietam co zrobilem

--- src/Index-components/ProductList.js
+++ src/Index-components/ProductList.js
@@ -1,18 +1,20 @@
 import '../Index-css/Products.css'
 import Grid from '@mui/material/Grid'; // Grid version 1
 import {Box} from '@mui/material';
-import { left } from '@popperjs/core';
+import { bottom, left } from '@popperjs/core';
 
 const ProductList = ({ products }) => {
     return (
         <>
             {products.map((product) => (
                 <Grid item className='product-item' width="calc(100% / 4)" height="30vh" 
-                border="1px solid blue" margin="10px 20px" 
+                 margin="10px 20px" justifyContent={'center'} borderRadius={6}
                 >
                     <img className='product-img' src={product.image}/>
-                    <Box>{product.name}</Box>
-                    <Box textAlign={left}>{product.price + " zł"}</Box>
+                    <Box textAlign={left}>
+                        <Box fontSize={15} paddingBottom={5}>{product.name}</Box>
+                        <Box>{product.price + " zł"}</Box>
+                    </Box>
                 </Grid>
             ))}
         </>
--- src/Index-components/Products.js
+++ src/Index-components/Products.js
@@ -2,7 +2,7 @@ import ProductList from "./ProductList";
 import { useState } from "react";
 import {BsLaptop} from 'react-icons/bs'
 import Grid from '@mui/material/Grid'; // Grid version 1
-import iphone13 from '../images/iphone13.jpg';
+import iphone13 from '../images/iphone13.png';
 import redmi from '../images/redmi.png';
 
 
@@ -10,18 +10,18 @@ import redmi from '../images/redmi.png';
 const Products = () => {
 
     const [products, setProduct] = useState([
-        { name: 'Iphone 14',description: 'Super telefon', price: 2137 ,image: iphone13,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 1},
-        { name: 'Iphone 13',description: 'Super telefon', price: 2137 ,image: redmi,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 2},
+        { name: 'Iphone 14/64GB/Pro',description: 'Super telefon', price: 2137 ,image: iphone13,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 1},
+        { name: 'Iphone 13',description: 'Super telefon', price: 2137 ,image: iphone13,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 2},
         { name: 'Samsung',description: 'TV', price: 2137 ,image: <BsLaptop/>,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 3},
         { name: 'Iphone 13',description: 'Super telefon', price: 2137 ,image: <BsLaptop/>,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 4},
         { name: 'Iphone 13',description: 'Super telefon', price: 2137 ,image: <BsLaptop/>,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 5},
         { name: 'Iphone 13',description: 'Super telefon', price: 2137 ,image: <BsLaptop/>,amount: 100, category: 'Smartphones',manufacturer:'Apple' ,id: 5},
-        
+  
     ]);
 
     return ( 
         <>
-        <Grid container spacing={2} height="70vh" maxWidth={1202} margin="auto" border="1px solid red" justifyContent="center" textAlign="center">
+        <Grid container spacing={2} height="70vh" maxWidth={1202} margin="auto" justifyContent="center" textAlign="center" borderTop="1px solid white" marginTop={5}>
             <ProductList products={products}/>
         </Grid>
         </>

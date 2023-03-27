5de506bcbf718c9ae99bb25b5a73ef6a40e8f729
MICHU_KFC Laptop <michal.pieciun@protonmail.com>
2023-03-27 20:20:40 +0200

    Wstępna podstrona Pomocy

--- src/pages/Lista/Lista.js
+++ src/pages/Lista/Lista.js
@@ -2,6 +2,8 @@ import NavBar from "../../Index-components/Navbar";
 import Categories from "../../Index-components/Categories";
 import Footer from "../../Index-components/Footer";
 
+
+
 const Lista = () => {
     return ( 
         <>
